ddab29fef6e0291660525b30d6ad8917a42b0e9f
Julio Cesar Spanholi <jcspanholi30@hotmail.com>
2021-11-09 17:24:44 -0300

    Att

---
+++ cypress/pages/components/serverest.elements.js
@@ -0,0 +1,30 @@
+export const LOGIN = {
+    INP_EMAIL : '#email',
+    INP_PASSWORD : '#password',
+    BTN_LOGIN : '[data-testid="entrar"]'
+}
+
+export const BODY_PROD = {
+    BTN_CADASTRAR : '[data-testid=cadastrarProdutos]',
+    INP_NOME : '#nome',
+    INP_PRECO : '[data-testid=preco]',
+    INP_DESCRICAO : '[data-testid=descricao]',
+    INP_QUANTIDADE : '[data-testid=quantity]',
+    BTN_IMG: '[data-testid=imagem]',
+    BTN_CADASTRO : '[data-testid=cadastarProdutos]',
+    DIV_ERRO : '//*[@id="root"]/div/div/div/form/div[1]'
+}
+
+export const CAD_USUARIO = {
+    BTN_CADASTRE    : '.btn.btn-link',
+    IMG             : '.imagem',
+    URL             : '/cadastrarusuarios',
+    INP_NOME        : '#nome',
+    INP_EMAIL       : '#email',
+    INP_SENHA       : '#password',
+    ADD_NOME        : '#nome',
+    ADD_EMAIL       : '[data-testid=email]', 
+    ADD_SENHA       : '[data-testid=password]',
+    ADD_ADMIN       : '[data-testid=checkbox]',
+    FIN_CADASTRO    : '[data-testid=cadastrar]'
+}
